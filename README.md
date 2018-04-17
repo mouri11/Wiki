3b6a5baab9c3b74e760c9d62fe88efc4c7c6c791
Rohit Das <rohit.das950@gmail.com>
2018-04-17 12:00:50 +0530

    Wiki-Viewer: Created new Wiki Viewer
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wiki Page Viewer
+View any page on wikipedia by searching, or a random article. Used [Wiki](https://www.mediawiki.org/wiki/API:Main_page) API.
---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$("#search").on("click", function() {
+		var query = $("#query").val();
+		var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=";
+		$.getJSON(url + query, function(json) {
+			var page = json.query.pages;
+			$("#results").append("<h5 align='center'>Top 15 results for '"+ query +"'</h5>")
+			$.each(page, function(i) {
+				$("#results").append("<p class='results'><a href='https://en.wikipedia.org/?curid="+ page[i] +"' target='_blank'>"+page[i].title+"</a></p>");
+			});
+		});
+	});
+
+	$("#clear").on("click", function() {
+		$("#query").val("");
+		$("#results").text("");
+	});
+});

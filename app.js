55a5b2b458512e86e187c08e03ba373f8fc6508c
Rohit Das <rohit.das950@gmail.com>
2018-04-17 14:55:51 +0530

    app.js: Minor fixes and changes done
    1. On clicking search without any keyword, errors occured. Fixed it
    with an if statement.
    2. Added fadeIn() to search result elements(belong to .results class)
    
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
 	$("#search").on("click", function() {
 		$("#results").text("");
 		var query = $("#query").val();
-		var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=";
-		$.getJSON(url + query, function(json) {
-			var page = json.query.pages;
-			$("#results").append("<h5 align='center'>Top 15 results for '"+ query +"'</h5>")
-			$.each(page, function(i) {
-				$("#results").append("<p class='results'><a href='https://en.wikipedia.org/?curid="+ page[i] +"' target='_blank'>"+page[i].title+"</a></p>");
+		if (!query) {
+			alert("Please enter a keyword");
+		}
+		else {
+			var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=";
+			$.getJSON(url + query, function(json) {
+				var page = json.query.pages;
+				$("#results").append("<h5 align='center'>Top 15 results for '"+ query +"'</h5>");
+				$.each(page, function(i) {
+					$("#results").append("<p class='results' id='"+ page[i].pageid +"'><a href='https://en.wikipedia.org/?curid="+ page[i].pageid +"' target='_blank'>"+page[i].title+"</a></p>");
+					$("#"+page[i].pageid).fadeIn(1000);
+				});
 			});
-		});
+		}
 	});
 
 	$("#clear").on("click", function() {
